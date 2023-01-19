3dc02f1fbc7a6b85a141882c533dc545d7232c49
Tesfaldet Weldemicheal <tweldemicheal@gmail.com>
2023-01-19 19:50:35 +0100

    commit the change

--- src/main/java/se/lexckon/jpaworkshop/Dao/Impl/AppUserDaoImpl.java
+++ src/main/java/se/lexckon/jpaworkshop/Dao/Impl/AppUserDaoImpl.java
@@ -1,33 +1,48 @@
 package se.lexckon.jpaworkshop.Dao.Impl;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import se.lexckon.jpaworkshop.Dao.AppUserDao;
 import se.lexckon.jpaworkshop.entity.AppUser;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@Repository
+
 public class AppUserDaoImpl implements AppUserDao {
+    @PersistenceContext
+    EntityManager entityManager;
 
     @Override
+    @Transactional
     public AppUser findById(int id) {
-        return null;
+        return entityManager.find(AppUser.class, id);
     }
 
     @Override
     public List<AppUser> findAll() {
-        return null;
+        return entityManager
+                .createQuery("select a from AppUser  a", AppUser.class)
+                .getResultList();
     }
 
     @Override
+    @Transactional
     public AppUser create() {
+
         return null;
     }
 
     @Override
+    @Transactional
     public AppUser uppDate() {
         return null;
     }
 
     @Override
+    @Transactional
     public void delete() {
 
     }
--- src/main/java/se/lexckon/jpaworkshop/Dao/Impl/DetailsDaoImpl.java
+++ src/main/java/se/lexckon/jpaworkshop/Dao/Impl/DetailsDaoImpl.java
@@ -1,19 +1,27 @@
 package se.lexckon.jpaworkshop.Dao.Impl;
 
+import org.springframework.stereotype.Repository;
 import se.lexckon.jpaworkshop.Dao.DetailsDao;
 import se.lexckon.jpaworkshop.entity.Details;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@Repository
 public class DetailsDaoImpl implements DetailsDao {
+    @PersistenceContext
+    EntityManager entityManager;
     @Override
     public Details findById(int id) {
-        return null;
+        return entityManager.find(Details.class, id);
     }
 
     @Override
     public List<Details> findAll() {
-        return null;
+        return entityManager
+                .createQuery("select d from Details d", Details.class)
+                .getResultList();
     }
 
     @Override
